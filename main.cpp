37c2024b912b1f649d54d0725a2f41bc830dd9d4
Catherine-Li-Xinran <63232799+Catherine-Li-Xinran@users.noreply.github.com>
2020-05-05 18:45:54 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Welcome to this game!" << endl;
+  cout << "Here you are at the fish pond."<< endl;
+  cout << "Go fishing: Press 1" << endl;
+  cout << "Go to the market (sell fish): Press 2"<< endl;
+  cout << "Go to the market (buy bait): Press 3"<< endl;
+  cout << "Exit the game: Press 4"<< endl;
+  cout << " "<< endl;
+  
+
+}
 
